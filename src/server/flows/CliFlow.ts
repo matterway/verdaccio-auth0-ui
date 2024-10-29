28bded5ca1cf0ad917edf24fd9fa9f407a4547d0
Anas <aaboudeine@matterway.io>
2024-10-29 14:38:32 +0100

    fix open redirect vulnerabilities

--- src/server/flows/CliFlow.ts
+++ src/server/flows/CliFlow.ts
@@ -1,67 +1,80 @@
-import { IPluginMiddleware } from "@verdaccio/types"
-import { Application, Handler, Request } from "express"
+import { IPluginMiddleware } from "@verdaccio/types";
+import { Application, Handler, Request } from "express";
 
-import { logger } from "../../logger"
-import { AuthCore } from "../plugin/AuthCore"
-import { AuthProvider } from "../plugin/AuthProvider"
-import { Verdaccio } from "../verdaccio"
-import { errorPage, WebFlow } from "./WebFlow"
+import { logger } from "../../logger";
+import { AuthCore } from "../plugin/AuthCore";
+import { AuthProvider } from "../plugin/AuthProvider";
+import { Verdaccio } from "../verdaccio";
+import { errorPage, WebFlow } from "./WebFlow";
+import { URL } from "url";
 
-const cliAuthorizeUrl = "/oauth/authorize"
-const cliCallbackUrl = "http://localhost:8239?token="
-const providerId = "cli"
+const cliAuthorizeUrl = "/oauth/authorize";
+const cliCallbackUrl = "http://localhost:8239?token=";
+const providerId = "cli";
 
-const pluginAuthorizeUrl = WebFlow.getAuthorizePath(providerId)
-const pluginCallbackeUrl = WebFlow.getCallbackPath(providerId)
+const pluginAuthorizeUrl = WebFlow.getAuthorizePath(providerId);
+const pluginCallbackUrl = WebFlow.getCallbackPath(providerId);
 
 export class CliFlow implements IPluginMiddleware<any> {
-
   constructor(
     private readonly verdaccio: Verdaccio,
     private readonly core: AuthCore,
-    private readonly provider: AuthProvider,
-  ) { }
+    private readonly provider: AuthProvider
+  ) {}
 
   /**
    * IPluginMiddleware
    */
   register_middlewares(app: Application) {
-    app.get(cliAuthorizeUrl, this.authorize)
-    app.get(pluginCallbackeUrl, this.callback)
+    app.get(cliAuthorizeUrl, this.authorize);
+    app.get(pluginCallbackUrl, this.callback);
   }
 
   authorize: Handler = async (req, res) => {
-    res.redirect(pluginAuthorizeUrl)
-  }
+    res.redirect(pluginAuthorizeUrl);
+  };
 
   callback: Handler = async (req, res, next) => {
     try {
-      const code = await this.provider.getCode(req)
-      const token = await this.provider.getToken(code, this.getRedirectUrl(req))
-      const username = await this.provider.getUsername(token)
-      const groups = await this.provider.getGroups(token)
+      const code = await this.provider.getCode(req);
+      const token = await this.provider.getToken(code, this.getRedirectUrl(req));
+      const username = await this.provider.getUsername(token);
+      const groups = await this.provider.getGroups(token);
 
       if (this.core.canAuthenticate(username, groups)) {
-        const npmToken = await this.verdaccio.issueNpmToken(username, token)
-        const cli = cliCallbackUrl + encodeURIComponent(npmToken)
-        res.redirect(cli)
+        const npmToken = await this.verdaccio.issueNpmToken(username, token);
+        const cli = cliCallbackUrl + encodeURIComponent(npmToken);
+        res.redirect(cli);
       } else {
-        res.send(errorPage)
+        res.send(errorPage);
       }
     } catch (error) {
-      logger.error(error)
-      next(error)
+      logger.error(error);
+      next(error);
     }
-  }
+  };
 
-  private getRequestOrigin(req: Request) {
-    const protocal = req.get("X-Forwarded-Proto") || req.protocol
-    return protocal + "://" + req.get("host")
+  /**
+   * Gets the origin for requests, validating headers to prevent injection.
+   */
+  private getRequestOrigin(req: Request): string {
+    const protocol = req.get("X-Forwarded-Proto") || req.protocol;
+    const host = req.get("host");
+    const safeProtocol = protocol === "http" || protocol === "https" ? protocol : "http";
+    const safeHost = host && /^[\w.-]+$/.test(host) ? host : "localhost";
+
+    return `${safeProtocol}://${safeHost}`;
   }
 
   private getRedirectUrl(req: Request): string {
-    const baseUrl = this.verdaccio.baseUrl || this.getRequestOrigin(req)
-    const path = WebFlow.getCallbackPath(providerId)
-    return baseUrl + path
+    const baseUrl = this.verdaccio.baseUrl || this.getRequestOrigin(req);
+    const path = WebFlow.getCallbackPath(providerId);
+    const parsedUrl = new URL(path, baseUrl);
+    const VALID_REDIRECT_PATHS = new Set(["/oauth/authorize", "/oauth/callback", "/auth/error", "/"]);
+    if (!VALID_REDIRECT_PATHS.has(parsedUrl.pathname)) {
+      throw new Error("Invalid redirect path");
+    }
+
+    return parsedUrl.toString();
   }
 }
--- src/server/flows/WebFlow.ts
+++ src/server/flows/WebFlow.ts
@@ -1,41 +1,41 @@
-import { IPluginMiddleware } from "@verdaccio/types"
-import { Application, Handler, Request } from "express"
+import { IPluginMiddleware } from "@verdaccio/types";
+import { Application, Handler, Request } from "express";
+import { parse, format } from "url";
 
-import { authorizePath, callbackPath } from "../../constants"
-import { logger } from "../../logger"
-import { buildStatusPage } from "../../statusPage"
-import { AuthCore } from "../plugin/AuthCore"
-import { AuthProvider } from "../plugin/AuthProvider"
-import { Verdaccio } from "../verdaccio"
+import { authorizePath, callbackPath } from "../../constants";
+import { logger } from "../../logger";
+import { buildStatusPage } from "../../statusPage";
+import { AuthCore } from "../plugin/AuthCore";
+import { AuthProvider } from "../plugin/AuthProvider";
+import { Verdaccio } from "../verdaccio";
 
 export const errorPage = buildStatusPage(`
   <h1>Access Denied</h1>
   <p>Not enough permissions to access registry.</p>
   <p><a href="/">Go back</a></p>
-`)
+`);
 
 export class WebFlow implements IPluginMiddleware<any> {
-
   static getAuthorizePath(id?: string) {
-    return authorizePath + "/" + (id || ":id?")
+    return authorizePath + "/" + (id || ":id?");
   }
 
   static getCallbackPath(id?: string) {
-    return callbackPath + (id ? "/" + id : "")
+    return callbackPath + (id ? "/" + id : "");
   }
 
   constructor(
     private readonly verdaccio: Verdaccio,
     private readonly core: AuthCore,
-    private readonly provider: AuthProvider,
-  ) { }
+    private readonly provider: AuthProvider
+  ) {}
 
   /**
    * IPluginMiddleware
    */
   register_middlewares(app: Application) {
-    app.get(WebFlow.getAuthorizePath(), this.authorize)
-    app.get(WebFlow.getCallbackPath(), this.callback)
+    app.get(WebFlow.getAuthorizePath(), this.authorize);
+    app.get(WebFlow.getCallbackPath(), this.callback);
   }
 
   /**
@@ -43,14 +43,14 @@ export class WebFlow implements IPluginMiddleware<any> {
    */
   authorize: Handler = async (req, res, next) => {
     try {
-      const redirectUrl = this.getRedirectUrl(req)
-      const url = await this.provider.getLoginUrl(redirectUrl)
-      res.redirect(url)
+      const redirectUrl = this.getRedirectUrl(req);
+      const url = await this.provider.getLoginUrl(redirectUrl);
+      res.redirect(url);
     } catch (error) {
-      logger.error(error)
-      next(error)
+      logger.error(error);
+      next(error);
     }
-  }
+  };
 
   /**
    * After successful authentication, the auth provider redirects back to us.
@@ -68,32 +68,55 @@ export class WebFlow implements IPluginMiddleware<any> {
    */
   callback: Handler = async (req, res, next) => {
     try {
-      const code = await this.provider.getCode(req)
-      const token = await this.provider.getToken(code, this.getRedirectUrl(req))
-      const username = await this.provider.getUsername(token)
-      const groups = await this.provider.getGroups(token)
+      const code = await this.provider.getCode(req);
+      const token = await this.provider.getToken(code, this.getRedirectUrl(req));
+      const username = await this.provider.getUsername(token);
+      const groups = await this.provider.getGroups(token);
 
       if (this.core.canAuthenticate(username, groups)) {
-        const ui = await this.core.createUiCallbackUrl(username, groups, token)
-        res.redirect(ui)
+        const ui = await this.core.createUiCallbackUrl(username, groups, token);
+        res.redirect(ui);
       } else {
-        res.send(errorPage)
+        res.send(errorPage);
       }
     } catch (error) {
-      logger.error(error)
-      next(error)
+      logger.error(error);
+      next(error);
     }
-  }
+  };
 
+  /**
+   * Constructs the redirect URL and validates it against a whitelist.
+   */
   private getRedirectUrl(req: Request): string {
-    const baseUrl = this.verdaccio.baseUrl || this.getRequestOrigin(req)
-    const path = WebFlow.getCallbackPath(req.params.id)
-    return baseUrl + path
+    const baseUrl = this.verdaccio.baseUrl || this.getRequestOrigin(req);
+    const path = WebFlow.getCallbackPath(req.params.id);
+
+    // Parse and validate the redirect path
+    const parsedUrl = parse(path);
+    const VALID_REDIRECT_PATHS = new Set(["/oauth/authorize", "/oauth/callback", "/auth/error", "/"]);
+    if (!VALID_REDIRECT_PATHS.has(parsedUrl.pathname || "")) {
+      throw new Error("Invalid redirect path");
+    }
+
+    return format({
+      protocol: parsedUrl.protocol || baseUrl.split("://")[0],
+      host: parsedUrl.host || baseUrl.split("://")[1],
+      pathname: parsedUrl.pathname,
+    });
   }
 
-  private getRequestOrigin(req: Request) {
-    const protocal = req.get("X-Forwarded-Proto") || req.protocol
-    return protocal + "://" + req.get("host")
-  }
+  /**
+   * Gets the origin for requests, validating headers to prevent injection.
+   */
+  private getRequestOrigin(req: Request): string {
+    const protocol = req.get("X-Forwarded-Proto") || req.protocol;
+    const host = req.get("host");
 
+    // Whitelist accepted protocols and sanitize the host header
+    const safeProtocol = protocol === "http" || protocol === "https" ? protocol : "http";
+    const safeHost = host && /^[\w.-]+$/.test(host) ? host : "localhost";
+
+    return `${safeProtocol}://${safeHost}`;
+  }
 }
