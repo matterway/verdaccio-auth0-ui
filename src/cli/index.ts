79dc60ddbbbc6a3849ad0ee66a80c0823a0f1301
Anas <aaboudeine@matterway.io>
2024-10-29 15:01:07 +0100

    add CSP HSTS headers

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,32 +1,37 @@
-import express from "express"
-import open from "open"
+import express from "express";
+import open from "open";
 
-import { buildStatusPage } from "../statusPage"
-import { getConfigFile, getRegistry, save } from "./npm"
-import { printUsage } from "./usage"
+import { buildStatusPage } from "../statusPage";
+import { getConfigFile, getRegistry, save } from "./npm";
+import { printUsage } from "./usage";
 
-const registry = getRegistry()
+const registry = getRegistry();
 
-if (registry.includes("registry.npmjs.org")) { // lgtm [js/incomplete-url-substring-sanitization]
-  printUsage()
-  process.exit(1)
+if (registry.includes("registry.npmjs.org")) {
+  printUsage();
+  process.exit(1);
 }
 
-open(registry + "/oauth/authorize")
+open(`${registry}/oauth/authorize`);
 
 const successPage = buildStatusPage(`
   <h1>All done!</h1>
   <p>We've updated your npm configuration.</p>
   <p><code>${getConfigFile()}</code></p>
-`)
+`);
 
 const server = express()
-  .get("/", (req, res) => {
-    const token = decodeURIComponent(req.query.token)
-    save(registry, token)
-    res.setHeader("Content-Type", "text/html")
-    res.send(successPage)
-    server.close()
-    process.exit(0)
+  .use((req, res, next) => {
+    res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+    res.setHeader("Content-Security-Policy", "default-src 'self';");
+    next();
   })
-  .listen(8239)
+  .get("/", (req, res) => {
+    const token = decodeURIComponent(req.query.token as string);
+    save(registry, token);
+    res.setHeader("Content-Type", "text/html");
+    res.send(successPage);
+    server.close();
+    process.exit(0);
+  })
+  .listen(8239);
